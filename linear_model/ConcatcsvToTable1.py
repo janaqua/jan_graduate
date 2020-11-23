b8546a568e07979353cf0b18c8218e2dbe3786fe
janaqua <55695716+janaqua@users.noreply.github.com>
2020-11-23 11:08:45 +0800

    Delete all

--- linear_model/ConcatcsvToTable1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-table1 = pd.DataFrame()
-for i in [1,2,5,10,20,50,100,200]:
-
-    table1 = table1.append(pd.read_csv('ToConcatenate\Batch_ASGDandSGD_with_GammaDecay_df%d.csv'%i))
-
-table1.to_csv('Table1.csv', index=False)
